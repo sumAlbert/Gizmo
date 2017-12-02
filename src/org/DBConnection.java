3a529632d39ec19764b50bee4e6d4a1950f1d9ee
SuperDanielLiang <1406859565@qq.com>
2017-12-02 13:59:34 +0800

    数据库连接

---
+++ src/org/DBConnection.java
@@ -0,0 +1,41 @@
+package org;
+
+import javax.servlet.http.HttpSessionBindingEvent;
+import javax.servlet.http.HttpSessionBindingListener;
+import java.sql.*;
+
+public class DBConnection implements HttpSessionBindingListener{
+    Connection conn = null;
+    Statement stmt = null;
+    ResultSet rs = null;
+
+    public void valueBound(HttpSessionBindingEvent event) {
+    }
+
+    public void valueUnbound(HttpSessionBindingEvent event) {
+    }
+
+    public DBConnection() {
+        this.BuildConnection();
+    }
+
+    private void BuildConnection() {
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            String URL = "jdbc:mysql://localhost:3306/Gizmo?useUnicode=true&characterEncoding=utf-8";
+            conn = DriverManager.getConnection(URL,"root", "");
+        } catch(Exception e) {
+            System.out.println(e.toString());
+        }
+    }
+
+    public Statement getStatement() {
+        try {
+            stmt = conn.createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return stmt;
+    }
+
+}
